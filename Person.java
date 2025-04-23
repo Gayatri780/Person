9f86ed968113ded1c63129df122c6fefeb29855b
Gayatri780 <gayatriapatil2003@gmail.com>
2025-04-23 14:03:52 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,18 @@
+package Pack1;
+
+public class Person {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int id;
+		String name;
+		String qualif;
+		void hobby();
+        {
+          System.out.println("The employee swim");
+		}
+		
+
+	}
+
+}
